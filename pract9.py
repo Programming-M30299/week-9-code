7917beb86113e649c3716a15fd2c25a4bced9f6d
manighahrmani <mashapicasso@gmail.com>
2023-07-13 15:43:51 +0100

    Update pract9.py

--- pract9.py
+++ pract9.py
@@ -1,26 +1,21 @@
-primes = [2, 3, 5, 7, 11, 13, 17, 19]
-primes[0]  # 2
-primes[7]  # 19
-primes[8]  # IndexError: list index out of range
-primes[-1]  # 19
-primes[-9]  # IndexError: list index out of range
+def displayMonths():
+    months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun",
+              "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+    for month in months:
+        print(month)
 
-weekdays = ["Mon", "Tue", "Wed", "Thu", "Fri"]
-weekdays[1] = "Tue"
 
-temperatures = [14.5, 8.0, -2.5, 15.0]
-temperatures[-2] = 10.0
+def displayTemperatureOfWeek():
+    temperatures = [14.5, 8.0, -2.5, 15.0, 12.5, 9.0, -1.0]
+    for temp in temperatures:
+        print("It's {} degrees today".format(temp))
+        if temp < 0:
+            print("Brrrrr, that's freezing!")
 
-type(primes)  # <class 'list'>
-type(weekdays)  # <class 'list'>
-type(temperatures)  # <class 'list'>
 
-[2, 4, 6, 8] * 3  # [2, 4, 6, 8, 2, 4, 6, 8, 2, 4, 6, 8]
-[0] * 3  # [0, 0, 0]
-weekdays * 2  # ['Mon', 'Tue', 'Wed', 'Thu', 'Mon', 'Tue', 'Wed', 'Thu']
-
-weekend = ["Sat", "Sun"]
-daysOfWeek = weekdays + weekend
-daysOfWeek  # ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
-
-primes2 = primes + [23, 29]
+def processNumbers():
+    numbers = [11, 28, 32, 34, 45, 67, 70, 89, 90, 99]
+    for i in range(len(numbers)):
+        if i % 2 == 0:  # Only process numbers at even indexes
+            square = numbers[i] ** 2
+            print("The square of {} is {}".format(numbers[i], square))
